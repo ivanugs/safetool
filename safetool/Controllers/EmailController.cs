6a470ec8d57cc4f6cec1f6d35dda97bdd6a50120
Tecnico.Seg.SA AU_ZU_FA <uig65332@contiwan.com>
2024-10-25 14:26:43 -0600

    Style

--- safetool/Controllers/EmailController.cs
+++ safetool/Controllers/EmailController.cs
@@ -31,5 +31,72 @@ namespace safetool.Controllers
 
             return RedirectToAction("Details", "Devices", new { id = deviceID });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmailSuccess(string subject, int deviceID)
+        {
+            var device = await _context.Devices.FindAsync(deviceID);
+            var fullName = @User.Claims.FirstOrDefault(c => c.Type == "FullName")?.Value;
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+            var deviceName = device.Name;
+
+            string body = $@"
+            <!DOCTYPE html>
+                <html lang='es'>
+                <head>
+                    <meta charset='UTF-8'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                    <title>Registro Exitoso</title>
+                    <style>
+                        body {{
+                            font-family: Arial, sans-serif;
+                            margin: 0;
+                            padding: 20px;
+                        }}
+                        .container {{
+                            max-width: 600px;
+                            margin: 0 auto;
+                            padding: 20px;
+                            border-radius: 8px;
+                            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+                        }}
+                        h1 {{
+                            color: #333;
+                            font-size: 20px;
+                        }}
+                        p {{
+                            color: #555;
+                            font-size: 16px;
+                            line-height: 1.5;
+                        }}
+                        strong {{
+                            color: #333;
+                        }}
+                        .footer {{
+                            margin-top: 20px;
+                            font-size: 14px;
+                            color: #777;
+                        }}
+                    </style>
+                </head>
+                <body>
+                    <div class='container'>
+                        <h1>{fullName},</h1>
+                        <p>
+                            Usted ha realizado exitosamente el registro del equipo: <strong>{deviceName}</strong>. <br> Su registro cuenta con una vigencia de 6 meses.
+                        </p>
+                        <p>Gracias por su atención.</p>
+                        <div class='footer'>
+                            <p>Saludos cordiales,<br>Equipo de ESH</p>
+                        </div>
+                    </div>
+                </body>
+                </html>";
+
+            await _emailService.SendEmailAsync(email, subject, body);
+
+            return RedirectToAction("Details", "Devices", new { id = deviceID });
+        }
     }
 }
--- safetool/Services/FormSubmissionService.cs
+++ safetool/Services/FormSubmissionService.cs
@@ -22,7 +22,7 @@ namespace safetool.Services
             // Obtener los registros que tienen más de 6 meses y que aún no han sido notificados
             var expiredSubmissions = await _context.FormSubmissions
                 .Include(f => f.Device)
-                .Where(f => f.CreatedAt.AddDays(6) <= DateTime.Now)
+                .Where(f => f.CreatedAt.AddMonths(6) <= DateTime.Now)
                 .ToListAsync();
 
             foreach (var submission in expiredSubmissions)
@@ -34,15 +34,71 @@ namespace safetool.Services
                 var deviceID = submission.Device.ID;
 
                 string subject = "Safetool - Tu registro ha vencido";
-                string body = $"Estimado {userName}, <br>" +
-                    $" Tu registro para el equipo '{device}' ha vencido. <br>" +
-                    $"Te invitamos a registrarte nuevamente en el siguiente enlace <a href='{deviceID}'>Safetool</a>." +
-                    $"<br><br>Gracias por su atención.<br>Saludos cordiales,<br>Equipo de ESH";
+                string body = $@"
+                <!DOCTYPE html>
+                <html lang='es'>
+                <head>
+                    <meta charset='UTF-8'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                    <title>Notificación de Registro Vencido</title>
+                    <style>
+                        body {{
+                            font-family: Arial, sans-serif;
+                            margin: 0;
+                            padding: 20px;
+                        }}
+                        .container {{
+                            max-width: 600px;
+                            margin: 0 auto;
+                            padding: 20px;
+                            border-radius: 8px;
+                            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+                        }}
+                        h1 {{
+                            color: #333;
+                            font-size: 20px;
+                        }}
+                        p {{
+                            color: #555;
+                            font-size: 16px;
+                            line-height: 1.5;
+                        }}
+                        a {{
+                            color: #007BFF;
+                            text-decoration: none;
+                        }}
+                        a:hover {{
+                            text-decoration: underline;
+                        }}
+                        .footer {{
+                            margin-top: 20px;
+                            font-size: 14px;
+                            color: #777;
+                        }}
+                    </style>
+                </head>
+                <body>
+                    <div class='container'>
+                        <h1>Estimado {userName},</h1>
+                        <p>
+                            Tu registro para el equipo <strong>'{device}'</strong> ha vencido.
+                        </p>
+                        <p>
+                            Te invitamos a registrarte nuevamente en el siguiente enlace: 
+                            <a href='https://safetool.conti.de/Devices/Details?id={deviceID}'>Safetool</a>.
+                        </p>
+                        <p>Gracias por su atención.</p>
+                        <div class='footer'>
+                            <p>Saludos cordiales,<br>Equipo de ESH</p>
+                        </div>
+                    </div>
+                </body>
+                </html>";
 
                 // Enviar correo
                 await _emailService.SendEmailAsync(userEmail, subject, body);
-
             }
+
         }
     }
 }
